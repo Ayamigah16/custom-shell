55f16c56ead2a019172304d0ef4db16fb6028a29
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-10-05 20:16:02 +0000

    added a feature to view the command history

--- main/main.py
+++ main/main.py
@@ -1,11 +1,33 @@
 import os
 
-while True:
-    user_input = input("Command: ")
-    if user_input.lower() == "exit":
-        break
+command_history = []
 
+def execute_command(command):
+    """Allows user to execute commands
+
+    Args:
+        command (str): command to excute
+    """
     try:
-        os.system(user_input)
+        output = os.popen().read()
     except Exception as e:
         print(f"An error occured: {str(e)}")
+
+def main():
+    while True:
+        user_input = input("Command: ")
+        if user_input.lower() == "exit":
+            break
+
+        command_history.append(user_input)
+
+        if user_input.lower() == "history":
+            print("Command History: ")
+            for idx, cmd in enumerate(command_history, start=1):
+                print(f"{idx}, {cmd}")
+        else:
+            execute_command(user_input)
+
+
+if __name__ == "__main__":
+    main()
